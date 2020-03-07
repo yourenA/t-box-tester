c9780dbd33ac65875f9261ade44bcabf7f5bcae7
yourena <2373638339@qq.com>
2020-03-07 18:30:43 +0800

    fix bug

--- src/Pre.js
+++ src/Pre.js
@@ -1,33 +1,86 @@
 import React, {PureComponent, Fragment} from 'react';
-import stats from './067-stats.png';
-import lottery from './130-lottery.png';
 import './App.css';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    useHistory
-} from "react-router-dom";
-import Button from '@material-ui/core/Button';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 import IconButton from '@material-ui/core/IconButton';
+import Divider from '@material-ui/core/Divider';
 import MenuIcon from '@material-ui/icons/ArrowBack';
+import Chip from '@material-ui/core/Chip';
 const {ipcRenderer} = window.electron;
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 class App extends PureComponent {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            drivers:[],
+            errorOpen:false,
+            errorName:'',
+            tBox:[{name:'T-Box-1'},{name:'T-Box-2'},{name:'T-Box-3'},{name:'T-Box-4'},{name:'T-Box-5'}
+            ,{name:'T-Box-6'},{name:'T-Box-7'},{name:'T-Box-8'}]
+        };
     }
 
     componentDidMount() {
-        ipcRenderer.on('ping', function(event, message) {
-            console.log('测试页接受到主进程发送过来的消息',message);  // Prints "whoooooooh!"
+        const that=this;
+        ipcRenderer.send('getDrivers')
+        ipcRenderer.on('getDriversFromMain', function(event, message) {
+            console.log('获取到的驱动',message)
+            for(let i=0;i<message.length;i++){
+                message[i].checked=false
+            }
+            that.setState({
+                drivers:message,
+            })
         });
-    }
+        ipcRenderer.on('openDriversFromMain', function(event, openResult,index,item) {
+            console.log('打开驱动',openResult,index,item)
+            if(openResult===0){
+                for(let i=0;i<that.state.drivers.length;i++){
+                    if(i==index){
+                        that.state.drivers[i].checked=!that.state.drivers[i].checked;
+                        that.setState({
+                            drivers:[...that.state.drivers],
+                        })
+                    }
+                }
+            }else{
+                that.setState({
+                    errorOpen:true,
+                    errorName:item.name
+                })
 
+            }
+        });
+
+    }
+    handleChangeCheck=(index,item)=>{
+        const that=this;
+        if(item.checked){
+            console.log('已经打开驱动')
+            for(let i=0;i<that.state.drivers.length;i++){
+                if(i==index){
+                    that.state.drivers[i].checked=!that.state.drivers[i].checked;
+                    that.setState({
+                        drivers:[...that.state.drivers],
+                    })
+                }
+            }
+        }else{
+            ipcRenderer.send('openDrivers',index,item);
+        }
+
+
+
+    }
     render() {
         return (
             <div>
@@ -45,8 +98,94 @@ class App extends PureComponent {
                     </Toolbar>
                 </AppBar>
                 <div className="test-page">
-                </div>
+                        <div className="drivers">
+                            <p className={'title'}>可选驱动</p>
+                            <div style={{marginTop:'12px'}}>
+                                { this.state.drivers.map((item,index)=>{
+                                    return(
+                                        <Chip
+                                            clickable
+                                            onClick={()=>this.handleChangeCheck(index,item)}
+                                            key={index}
+                                            label={item.name}
+                                            color={item.checked?"primary":"default"}
+                                        />
+                                    )
+                                })}
 
+                            </div>
+                        {/*    <FormGroup row>
+                                {
+                                    this.state.drivers.map((item,index)=>{
+                                        return     <FormControlLabel
+                                            key={index}
+                                            control={
+                                                <Checkbox
+                                                    onChange={()=>this.handleChangeCheck(index,item)}
+                                                    checked={item.checked}
+                                                    value={item.library}
+                                                    color="primary"
+                                                />
+                                            }
+                                            label={item.name}
+                                        />
+                                    })
+                                }
+
+                            </FormGroup>*/}
+                        </div>
+                    <Divider light/>
+                    <div className="drivers">
+                        <p className={'title'}>T-Box抽屉  <Checkbox
+                            defaultChecked
+                            color="primary"
+                        /></p>
+                         <FormGroup row>
+                                {
+                                    this.state.tBox.map((item,index)=>{
+                                        return     <FormControlLabel
+                                            key={index}
+                                            labelPlacement="bottom"
+                                            control={
+                                                <Checkbox
+
+                                                    value={item.name}
+                                                    color="primary"
+                                                />
+                                            }
+                                            label={item.name}
+                                        />
+                                    })
+                                }
+                       {
+                           this.state.tBox.map((item,index)=>{
+                               return     <FormControlLabel
+                                   key={index}
+                                   labelPlacement="bottom"
+                                   control={
+                                       <Checkbox
+
+                                           value={item.name}
+                                           color="primary"
+                                       />
+                                   }
+                                   label={item.name}
+                               />
+                           })
+                       }
+
+                            </FormGroup>
+                    </div>
+                    <Divider light />
+                </div>
+                <Snackbar open={this.state.errorOpen}  anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                          autoHideDuration={6000} onClose={()=>{
+                    this.setState({
+                        errorOpen:false
+                    })
+                }}>
+                    <Alert severity="error">打开驱动{this.state.errorName}失败</Alert>
+                </Snackbar>
             </div>
 
         );
